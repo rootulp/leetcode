e7037147b101e7d5156a818c0d2dd6aaa8fac484
Rootul Patel <rootulp@amazon.com>
2021-04-14 18:08:11 -0400

    Partial impl of moving pointers
    I think a better approach would be to iterate through the list once,
    store the values in an array, and then construct a new linked list.

--- typescript/partition-list.ts
+++ typescript/partition-list.ts
@@ -10,9 +10,33 @@ class ListNode {
 
 
 function partition(head: ListNode | null, x: number): ListNode | null {
+    let nodesLessThanX = null;
+    const tip = head;
+    let current = head;
+    while (current.next != null) {
+        console.log(`current.next != null`)
+        console.log(`nodesLessThanX: ${JSON.stringify(nodesLessThanX)}`)
+        if (current.next.val < x ) {
+            if (nodesLessThanX == null) {
+                nodesLessThanX = current.next;
+            } else {
+                let lastNodeLessThanX = nodesLessThanX;
+                while (lastNodeLessThanX.next != null) {
+                    console.log(`lastNodeLessThanX.next != null`)
+                    lastNodeLessThanX = lastNodeLessThanX.next
+                }
+                lastNodeLessThanX.next = current.next;
+                lastNodeLessThanX.next.next = null;
+            }
+            current.next = current.next.next;
+        }
+        current = current.next;
+    }
+    console.log(`current: ${JSON.stringify(current)}`)
+    console.log(`nodesLessThanX: ${JSON.stringify(nodesLessThanX)}`)
     return new ListNode(3);
 };
 
-const test = new ListNode(2, new ListNode(1))
-const result = partition(test, 2)
+const test = new ListNode(1, new ListNode(4, new ListNode(3, new ListNode(2, new ListNode(5, new ListNode(2))))))
+const result = partition(test, 3)
 console.log(`result: ${JSON.stringify(result)}`)
