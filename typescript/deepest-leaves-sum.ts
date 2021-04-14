289a34a1ae29577905b19c5678e303007638875e
Rootul Patel <rootulp@amazon.com>
2021-04-14 17:43:09 -0400

    Uncomment TreeNode definition

--- typescript/deepest-leaves-sum.ts
+++ typescript/deepest-leaves-sum.ts
@@ -1,16 +1,15 @@
-/**
- * Definition for a binary tree node.
- * class TreeNode {
- *     val: number
- *     left: TreeNode | null
- *     right: TreeNode | null
- *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.left = (left===undefined ? null : left)
- *         this.right = (right===undefined ? null : right)
- *     }
- * }
- */
+//   Definition for a binary tree node.
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.left = (left===undefined ? null : left)
+        this.right = (right===undefined ? null : right)
+    }
+}
+
 
 function deepestLeavesSum(root: TreeNode | null): number {
   const maxDepth = getMaxDepth(root)
@@ -30,7 +29,7 @@ function getSumAtDepth(node: TreeNode, currentDepth: number, maxDepth: number) {
     }
     if (currentDepth === maxDepth) {
         return node.val
-    } 
-    return getSumAtDepth(node.left, currentDepth + 1, maxDepth) + 
+    }
+    return getSumAtDepth(node.left, currentDepth + 1, maxDepth) +
            getSumAtDepth(node.right, currentDepth + 1, maxDepth)
 }
