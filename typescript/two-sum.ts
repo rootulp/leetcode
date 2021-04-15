c53d6424909179e5bed0873d809c67e554025f44
Rootul Patel <rootulp@amazon.com>
2021-04-15 19:10:13 -0400

    Solve two sum

---
+++ typescript/two-sum.ts
@@ -0,0 +1,12 @@
+function twoSum(nums: number[], target: number): number[] {
+    for (let i = 0; i < nums.length;  i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (i == j) {
+                continue
+            }
+            if (nums[i] + nums[j] === target) {
+                return [i, j]
+            }
+        }
+    }
+};
