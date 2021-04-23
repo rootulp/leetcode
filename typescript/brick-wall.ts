fb58cf18e29a07385eae3e4a1d96cdcad9b00164
Rootul Patel <rootulp@amazon.com>
2021-04-22 20:33:41 -0400

    Solve brick wall using dict of edges

--- typescript/brick-wall.ts
+++ typescript/brick-wall.ts
@@ -1,44 +1,26 @@
 function leastBricks(wall: number[][]): number {
-    const wallWidth = sum(wall[0]);
+    // const wallWidth = sum(wall[0]);
     // console.log(`wallWidth ${wallWidth}`);
+    let maxEdges = 0;
 
-    let bricksIntersected = [];
-    for (let i = 1; i < wallWidth; i++) {
-        bricksIntersected.push(bricksIntersectedBy(wall, i));
-    }
-
-    // console.log(`bricksIntersected: ${bricksIntersected}`);
-    return Math.min(...bricksIntersected)
-};
-
-function bricksIntersectedBy(wall: number[][], index: number): number {
-    let sum = 0;
+    const indexToNumEdges = new Map<Number, number>();
     for (let row = 0; row < wall.length; row += 1) {
-        if (doesIntersectRow(wall[row], index)) {
-            sum += 1;
+        let index = 0;
+
+        for (let col = 0; col < wall[row].length - 1; col += 1) {
+            index += wall[row][col]
+            if (!indexToNumEdges.has(index)) {
+                indexToNumEdges.set(index, 0);
+            }
+            indexToNumEdges.set(index, indexToNumEdges.get(index) + 1);
+            maxEdges = Math.max(maxEdges, indexToNumEdges.get(index))
         }
     }
-    return sum;
-}
+    const result = wall.length - maxEdges;
 
-function doesIntersectRow(row: number[], index: number): boolean {
-    const edges = getEdges(row)
-    // console.log(`row: ${row}, edges: ${edges}`)
-    return !edges.includes(index);
-}
-
-function getEdges(row: number[]): number[] {
-    let edges = []
-    for (let i = 0; i < row.length; i++) {
-        const currentWallWidth = sum(row.slice(0, i))
-        edges.push(currentWallWidth + row[i])
-    }
-    return edges;
-}
-
-function sum(array: number[]): number {
-    if (array.length === 0 ) {
-        return 0;
-    }
-    return array.reduce((x, y) => x + y);
-}
+    console.log(`indexToNumEdges: ${JSON.stringify([...indexToNumEdges.entries()])}`);
+    console.log(`wall.length: ${wall.length}`);
+    console.log(`maxEdges: ${maxEdges}`);
+    console.log(`result: ${result}`)
+    return result;
+};
