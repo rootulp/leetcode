763b54483f5dee6623db9b527e8b131f527bb38c
Rootul Patel <rootulp@amazon.com>
2021-04-24 19:49:44 -0400

    Solve integer break

---
+++ typescript/integer-break.ts
@@ -0,0 +1,15 @@
+function integerBreak(n: number): number {
+    if (n === 2) {
+        return 1;
+    }
+    if (n === 3) {
+        return 2;
+    }
+    let product = 1;
+    while (n > 4) {
+        product *= 3;
+        n -= 3;
+    }
+    product *= n;
+    return product;
+};
