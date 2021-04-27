e937d4a13b2850335b8f7b9759e2743e5f401a66
Rootul Patel <rootulp@amazon.com>
2021-04-26 21:42:07 -0400

    Remove commented code

--- typescript/furthest-building-you-can-reach.ts
+++ typescript/furthest-building-you-can-reach.ts
@@ -1,12 +1,12 @@
 function furthestBuilding(heights: number[], bricks: number, ladders: number): number {
-    const traverseBuildings = new TraverseBuildings(heights, bricks, ladders);
+    const traverseBuildings = new TraverseBuilding(heights, bricks, ladders);
     return traverseBuildings.getFurthestBuilding();
 };
 
-class TraverseBuildings {
-
-
-    constructor(private readonly heights: number[], private readonly bricks: number, private readonly ladders: number) { }
+class TraverseBuilding {
+    constructor(private readonly heights: number[],
+                private readonly bricks: number,
+                private readonly ladders: number) {}
 
     public getFurthestBuilding(): number {
         const minHeap = new MinHeap();
@@ -24,9 +24,7 @@ class TraverseBuildings {
                 continue;
             } else {
                 const smallestJump = minHeap.remove()
-                // console.log(`smallestJump: ${smallestJump}, minHeap: ${minHeap.heap}`)
                 bricksUsed += smallestJump;
-                // console.log(`bricksUsed: ${bricksUsed}, minHeap: ${minHeap.heap}, position ${position}, smallestJump: ${smallestJump}`);
                 if (bricksUsed > this.bricks) {
                     return position;
                 } else {
@@ -49,24 +47,15 @@ class MinHeap {
     }
 
     public getMin(): number {
-        /* Accessing the min element at index 1 in the heap array */
         return this.heap[1]
     }
 
     public insert(node: number) {
-
-        /* Inserting the new node at the end of the heap array */
         this.heap.push(node)
-
-        /* Finding the correct position for the new node */
 
         if (this.heap.length > 1) {
             let current = this.heap.length - 1
-
-            /* Traversing up the parent node until the current node (current) is greater than the parent (current/2)*/
             while (current > 1 && this.heap[Math.floor(current/2)] > this.heap[current]) {
-
-                /* Swapping the two nodes by using the ES6 destructuring syntax*/
                 [this.heap[Math.floor(current/2)], this.heap[current]] = [this.heap[current], this.heap[Math.floor(current/2)]]
                 current = Math.floor(current/2)
             }
@@ -74,12 +63,7 @@ class MinHeap {
     }
 
     public remove(): number {
-        /* Smallest element is at the index 1 in the heap array */
         let smallest = this.heap[1]
-
-        /* When there are more than two elements in the array, we put the right most element at the first position
-            and start comparing nodes with the child nodes
-        */
         if (this.heap.length > 2) {
             this.heap[1] = this.heap[this.heap.length-1]
             this.heap.splice(this.heap.length - 1)
@@ -111,8 +95,6 @@ class MinHeap {
                 rightChildIndex = current * 2 + 1
             }
         }
-
-        /* If there are only two elements in the array, we directly splice out the first element */
 
         else if (this.heap.length === 2) {
             this.heap.splice(1, 1)
