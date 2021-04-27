2e928f7e537419a9ff10c9cd071b75438bbfef7a
Rootul Patel <rootulp@amazon.com>
2021-04-26 20:43:45 -0400

    Move into class

--- typescript/furthest-building-you-can-reach.ts
+++ typescript/furthest-building-you-can-reach.ts
@@ -1,31 +1,42 @@
 function furthestBuilding(heights: number[], bricks: number, ladders: number): number {
-    return getFurthestBuilding(heights, bricks, ladders, 0);
+    const traverseBuildings = new TraverseBuildings(heights);
+    return traverseBuildings.getFurthestBuilding(bricks, ladders, 0);
 };
 
-function getFurthestBuilding(heights: number[], bricks: number, ladders: number, position: number): number {
-    // console.log(`bricks: ${bricks}, ladders: ${ladders}, position ${position}`);
-    if (position === heights.length - 1) {
-        return position
-    }
-    if (heights[position] >= heights[position + 1]) {
-        return getFurthestBuilding(heights, bricks, ladders, position + 1);
+
+class TraverseBuildings {
+
+    private readonly heights: number[];
+
+    constructor(heights: number[]) {
+        this.heights = heights;
     }
 
-    const bricksNeeded = heights[position + 1] - heights[position];
-    const canUseBricks = bricks >= bricksNeeded;
-    const canUseLadders = ladders > 0;
-    if (!canUseBricks && !canUseLadders ) {
-        return position;
-    }
+    public  getFurthestBuilding(bricks: number, ladders: number, position: number): number {
+        // console.log(`bricks: ${bricks}, ladders: ${ladders}, position ${position}`);
+        if (position === this.heights.length - 1) {
+            return position
+        }
+        if (this.heights[position] >= this.heights[position + 1]) {
+            return this.getFurthestBuilding(bricks, ladders, position + 1);
+        }
 
-    let advanceWithBricks = 0;
-    let advanceWithLadder = 0;
-    if (canUseBricks) {
-        advanceWithBricks = getFurthestBuilding(heights, bricks - bricksNeeded, ladders, position + 1);
-    }
-    if (canUseLadders) {
-        advanceWithLadder = getFurthestBuilding(heights, bricks, ladders - 1, position + 1);
-    }
+        const bricksNeeded = this.heights[position + 1] - this.heights[position];
+        const canUseBricks = bricks >= bricksNeeded;
+        const canUseLadders = ladders > 0;
+        if (!canUseBricks && !canUseLadders ) {
+            return position;
+        }
 
-    return Math.max(advanceWithBricks, advanceWithLadder);
+        let advanceWithBricks = 0;
+        let advanceWithLadder = 0;
+        if (canUseBricks) {
+            advanceWithBricks = this.getFurthestBuilding(bricks - bricksNeeded, ladders, position + 1);
+        }
+        if (canUseLadders) {
+            advanceWithLadder = this.getFurthestBuilding(bricks, ladders - 1, position + 1);
+        }
+
+        return Math.max(advanceWithBricks, advanceWithLadder);
+    }
 }
