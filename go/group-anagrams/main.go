5d9fdc8faa25dd160d49c66328e12f302804f4ca
Rootul Patel <rootulp@gmail.com>
2022-02-01 12:51:51 -0500

    Solve two test cases

--- go/group-anagrams/main.go
+++ go/group-anagrams/main.go
@@ -1,5 +1,42 @@
 package main
 
-func groupAnagrams(strs []string) [][]string {
-	return [][]string{{"foo"}}
+import (
+	"fmt"
+	"sort"
+)
+
+func groupAnagrams(words []string) (result [][]string) {
+	sortedToAnagrams := map[string][]string{}
+
+	for _, word := range words {
+		sorted := SortString(word)
+		_, ok := sortedToAnagrams[sorted]
+		if !ok {
+			sortedToAnagrams[sorted] = []string{}
+		}
+		sortedToAnagrams[sorted] = append(sortedToAnagrams[sorted], word)
+	}
+	fmt.Printf("sortedToAnagrams %v\n", sortedToAnagrams)
+
+	for _, anagrams := range sortedToAnagrams {
+		result = append(result, anagrams)
+	}
+	return result
+}
+
+type sortRunes []rune
+
+func (s sortRunes) Less(i, j int) bool {
+	return s[i] < s[j]
+}
+func (s sortRunes) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+func (s sortRunes) Len() int {
+	return len(s)
+}
+func SortString(s string) string {
+	r := []rune(s)
+	sort.Sort(sortRunes(r))
+	return string(r)
 }
