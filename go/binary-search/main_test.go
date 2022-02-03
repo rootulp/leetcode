c81fc50f7198906394eb50266b732f39998930aa
Rootul Patel <rootulp@gmail.com>
2022-02-03 11:13:18 -0500

    Pass test for target that does not exist

--- go/binary-search/main_test.go
+++ go/binary-search/main_test.go
@@ -11,3 +11,13 @@ func TestSearch(t *testing.T) {
 		t.Errorf("search(%v, %v) got %v want %v", nums, target, got, want)
 	}
 }
+
+func TestSearchWithTargetThatDoesNotExist(t *testing.T) {
+	nums := []int{-1, 0, 3, 5, 9, 12}
+	target := 2
+	got := search(nums, target)
+	want := -1
+	if got != want {
+		t.Errorf("search(%v, %v) got %v want %v", nums, target, got, want)
+	}
+}
