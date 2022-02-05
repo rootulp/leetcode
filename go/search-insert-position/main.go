d2c2e6c1ea0596eb09534388e248a91c1e666637
Rootul Patel <rootulp@gmail.com>
2022-02-05 14:41:54 -0500

    Remove redundant conditionals

--- go/search-insert-position/main.go
+++ go/search-insert-position/main.go
@@ -1,7 +1,5 @@
 package searchinsertposition
 
-import "fmt"
-
 // searchInsert returns the index of target value in nums. If target does not
 // exist in nums, returns the index at which target should be inserted to
 // preserve sorted order. nums must be provided in sorted order.
@@ -10,16 +8,11 @@ func searchInsert(nums []int, target int) (insertIndex int) {
 }
 
 func binarySearchInsert(nums []int, target int, low int, high int) (insertIndex int) {
-	fmt.Printf("binarySearchInsert(%v, %v, %v, %v)\n", nums, target, low, high)
 	if low >= high {
-		fmt.Printf("low >= high\n")
-		if target == nums[low] {
+		if target <= nums[low] {
 			return low
-		} else if target < nums[low] {
-			return low
-		} else if target > nums[low] {
-			return low + 1
 		}
+		return low + 1
 	}
 	middle := int((low + high) / 2)
 	middleVal := nums[middle]
