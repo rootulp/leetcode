7e1c3cb8b784be727c9b51d2169e06092ea52946
Rootul Patel <rootulp@gmail.com>
2022-02-04 23:08:23 -0500

    Solve first bad version

---
+++ go/first-bad-version/main.go
@@ -0,0 +1,36 @@
+package firstbadversion
+
+import "fmt"
+
+/**
+ * Forward declaration of isBadVersion API.
+ * @param   version   your guess about first bad version
+ * @return 	 	      true if current version is bad
+ *			          false if current version is good
+ * func isBadVersion(version int) bool;
+ */
+
+func firstBadVersion(n int) int {
+	return binarySearch(n, 0, n, n)
+}
+
+func binarySearch(n int, low int, high int, firstBadVersionSoFar int) (firstBadVersion int) {
+	fmt.Printf("binarySearch(%v, %v, %v, %v)\n", n, low, high, firstBadVersionSoFar)
+	if low > high {
+		return firstBadVersionSoFar
+	}
+	if low == high {
+		if isBadVersion(low) && low < firstBadVersionSoFar {
+			return low
+		}
+		return firstBadVersionSoFar
+	}
+	middle := int((high + low) / 2)
+	result := isBadVersion(middle)
+	fmt.Printf("middle %v is %v\n", middle, result)
+	if isBadVersion(middle) {
+		return binarySearch(n, low, middle-1, middle)
+	} else {
+		return binarySearch(n, middle+1, high, firstBadVersionSoFar)
+	}
+}
