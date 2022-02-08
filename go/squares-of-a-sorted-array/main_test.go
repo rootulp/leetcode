47e12ed6f0cab3babe7f42a68e61242b822498a4
Rootul Patel <rootulp@gmail.com>
2022-02-07 22:11:03 -0500

    Add failing test

--- go/squares-of-a-sorted-array/main_test.go
+++ go/squares-of-a-sorted-array/main_test.go
@@ -14,6 +14,7 @@ func TestSortedSquares(t *testing.T) {
 	tests := []test{
 		{[]int{-4, -1, 0, 3, 10}, []int{0, 1, 9, 16, 100}},
 		{[]int{-7, -3, 2, 3, 11}, []int{4, 9, 9, 49, 121}},
+		{[]int{-5, -3, -2, -1}, []int{1, 4, 9, 25}},
 	}
 	for _, test := range tests {
 		got := sortedSquares(test.input)
