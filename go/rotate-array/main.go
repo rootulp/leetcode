d86fc194c93d1761da64d91241ce1fabf7da0081
Rootul Patel <rootulp@gmail.com>
2022-02-09 12:07:52 -0500

    Use arithmetic to determine numToShift
    Resolves failure encountered by previous solution: Time Limit Exceeded

--- go/rotate-array/main.go
+++ go/rotate-array/main.go
@@ -1,18 +1,7 @@
 package main
 
 func rotate(nums []int, k int) {
-	rotated := make([]int, len(nums))
-	copy(rotated, nums)
-	for i := 0; i < k; i += 1 {
-		rotated = rotateOnce(rotated)
-	}
-
-	// this problem wants nums to be modified in place
-	// assigning nums = rotated would modify the pointer to nums
-	// therefore, we must copy rotated back into nums
+	numToShift := len(nums) - (k % len(nums))
+	rotated := append(nums[numToShift:], nums[0:numToShift]...)
 	copy(nums, rotated)
-}
-
-func rotateOnce(nums []int) (rotated []int) {
-	return append([]int{nums[len(nums)-1]}, nums[0:len(nums)-1]...)
 }
