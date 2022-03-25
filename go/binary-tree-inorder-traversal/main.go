753fdf6b5f34b154d45867d3b982434016204df3
Rootul Patel <rootulp@gmail.com>
2022-03-24 22:52:54 -0400

    Solve binary tree inorder traversal

---
+++ go/binary-tree-inorder-traversal/main.go
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func inorderTraversal(root *TreeNode) []int {
+	return traverse(root, []int{})
+}
+
+func traverse(node *TreeNode, visited []int) []int {
+	if node == nil {
+		return visited
+	}
+	if node.Left != nil {
+		visited = traverse(node.Left, visited)
+	}
+	visited = append(visited, node.Val)
+	if node.Right != nil {
+		visited = traverse(node.Right, visited)
+	}
+	return visited
+}
